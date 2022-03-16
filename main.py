86c8365ad888d12b8bedd1bce61830b1231eba42
realdaveblanch <101751386+realdaveblanch@users.noreply.github.com>
2022-03-16 23:06:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random, time, TrollScammer
+
+if __name__ == "__main__":
+    debug: bool = False
+    instance = TrollScammer.TrollScammer()
+    instance.get_variables_by_cli()
+    instance.print_query_information()
+    try:
+        while True:
+            username: str = instance.create_username()
+            password: str = instance.create_password()
+            instance.create_request(username, password)
+            if debug:
+                time.sleep(0.5)
+            else:
+                time.sleep(random.randint(0, 0) / 10)
+    except KeyboardInterrupt as k:
+        print("")
